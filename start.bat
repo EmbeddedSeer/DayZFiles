85a51b3293fb8f4d578e9a98a828a8e373c055e6
EmbeddedSeer <142927983+EmbeddedSeer@users.noreply.github.com>
2024-04-23 21:37:34 -0600

    .bat file for dedicated server

---
+++ start.bat
@@ -0,0 +1,23 @@
+@echo off
+:start
+taskkill /im DayZServer_x64.exe /F
+::Time in seconds to wait before..
+timeout 10
+::Server name
+set serverName=Wolfpack RP
+::Server files location
+set serverLocation="C:\DayZServer_x64"
+::Server Port
+set serverPort=2302
+::Server config
+set serverConfig=serverDZ.cfg
+::Logical CPU cores to use (Equal or less than available)
+set serverCPU=4
+::Sets title for terminal (DONT edit)
+title %serverName% batch
+::DayZServer location (DONT edit)
+cd "%serverLocation%"
+echo (%time%) %serverName% started.
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start "DayZ Server" /min "c:\DayZServer\DayZServer_x64.exe" -config=%serverConfig% -port=2302 -profiles=Profilesfolder "-mod=@Survivor Animations;@Stack bullets to 1000;@Server_Information_Panel;@MuchFramework;@MRTsMREs;@KillReward;@InventoryPlusPlus;@GoreZ;@Dabs Framework;@CF" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+
