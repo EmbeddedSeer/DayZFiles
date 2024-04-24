d5a7ed55d558ea304a27dd2e4da1ad68ba79f313
EmbeddedSeer <142927983+EmbeddedSeer@users.noreply.github.com>
2024-04-23 21:43:43 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
+<!-- When you apply this bat file you have to set up your restarts through task scheduler. Its a simple process-->
+<!--Win key>task scheduler>Create Basic Task>name your restart>daily>what ever time you want and how long you wantt until task no longer happen happen>start a program>browse for bat>finish-->
+<!--Repeat as needed-->
+<!--https://www.youtube.com/watch?v=yf0y2FDKqls-->
+
 @echo off
 :start
 taskkill /im DayZServer_x64.exe /F
@@ -20,4 +25,3 @@ cd "%serverLocation%"
 echo (%time%) %serverName% started.
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
 start "DayZ Server" /min "c:\DayZServer\DayZServer_x64.exe" -config=%serverConfig% -port=2302 -profiles=Profilesfolder "-mod=@Survivor Animations;@Stack bullets to 1000;@Server_Information_Panel;@MuchFramework;@MRTsMREs;@KillReward;@InventoryPlusPlus;@GoreZ;@Dabs Framework;@CF" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
-
